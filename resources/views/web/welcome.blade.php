2dea770db831215e22fad13f501b9c3735104b00
Shakidevcom <webtotem.plesk@gmail.com>
2022-10-15 17:44:18 +0600

    web

--- resources/views/web/welcome.blade.php
+++ resources/views/web/welcome.blade.php
@@ -1,5 +1,15 @@
-@extends('layouts.landing1')
-
-@section('content')
-
-@endsection
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Best Fortuna</title>
+</head>
+<body>
+    <a href="/login">
+        <img src="/bestfortune/landing.png"  style="width: 100%">
+    </a>
+</body>
+</html>
