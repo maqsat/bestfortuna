af258a8713f197762faa93b60503a3124a95c78f
Maksat <batyrqul.maqsat@gmail.com>
2023-05-02 12:56:11 +0600

    fix exsel

--- resources/views/exports/users.blade.php
+++ resources/views/exports/users.blade.php
@@ -7,12 +7,15 @@
     <tr>
         <th>Контракт</th>
         <th>ФИО</th>
+        <th>Дата регистрации</th>
+        <th>Регистрация</th>
+        <th>Покупки</th>
         <th>Спонсор</th>
         <th>Статус</th>
         <th>Активация</th>
         <th>Накопительный PV</th>
         <th>Товарооборот PV</th>
-        <th>Месяц({{ \App\Facades\General::getMonthNameById($date->format('n')) }})</th>
+        <th>Месяц</th>
     </tr>
     </thead>
     <tbody>
@@ -20,6 +23,9 @@
         <tr>
             <td>{{ $user->id_number }}</td>
             <td>{{ $user->name }}</td>
+            <td>{{ $user->created_at->format('Y-m-d') }}</td>
+            <td>{{ \App\Models\Package::find($user->package_id)->cost }}</td>
+            <td>{{ \App\Facades\Hierarchy::orderSumOfMonth($date,Auth::user()->id) }}</td>
             <td>
                 @php $sponsor = \App\User::whereId($user->inviter_id)->first(); @endphp
                 @if(!is_null($sponsor)) {{ $sponsor->id_number }}@endif
@@ -33,7 +39,7 @@
             </td>
             <td>{{ \App\Facades\Balance::getIncomeBalance($user->id) }}</td>
             <td>{{ \App\Facades\Hierarchy::pvCounterAll($user->id) }}</td>
-            <td>{{ \App\Facades\Balance::getBalance($user->id) }}</td>
+            <td>{{ \App\Facades\General::getMonthNameById($date->format('n')-2) }}</td>
         </tr>
 @endforeach
 </tbody>
